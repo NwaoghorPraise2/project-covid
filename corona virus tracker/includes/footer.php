ede87df9dbe0256a0476d8f9f8370d1b71cac60f
Nwaoghor Praise C <52567426+NwaoghorPraise2@users.noreply.github.com>
2021-08-30 08:33:51 +0100

    Delete corona virus tracker directory

--- corona virus tracker/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<html>
-<footer>
-<center>
-  <div style ="height:100px; width:80%; background:lightblue;">
-  </div>
-</center>
-</footer>
-</html>
--- corona virus tracker/readme.txt
+++
@@ -1,6 +0,0 @@
-create a database "covid"
-Import the covid.sql file into it
-
-For admin section;
-Username = admin
-password = admin
